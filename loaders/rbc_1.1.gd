4dc673795eef6e39e269d98ad47f3b0e67f24d12
Binpuki <30415792+Binpuki@users.noreply.github.com>
2025-01-29 17:14:26 -0600

    Fix signing integer bug

--- loaders/rbc_1.1.gd
+++ loaders/rbc_1.1.gd
@@ -6,37 +6,37 @@ static func convert(reader : FileAccess) -> RubiChart:
 	chart.Difficulty = reader.get_32()
 	chart.ScrollSpeed = reader.get_float()
 	
-	var charter_length : int = sign_uint32(reader.get_32())	
+	var charter_length : int = int(reader.get_32())	
 	chart.Charter = reader.get_buffer(charter_length).get_string_from_utf8()
 	
-	var note_types_length : int = sign_uint32(reader.get_32())
+	var note_types_length : int = int(reader.get_32())
 	var note_types : PackedStringArray = []
 	for i in note_types_length:
-		var type_length : int = sign_uint32(reader.get_32())
+		var type_length : int = int(reader.get_32())
 		note_types.push_back(reader.get_buffer(type_length).get_string_from_utf8())
 	
-	var amtOfCharts : int = sign_uint32(reader.get_32())
+	var amtOfCharts : int = int(reader.get_32())
 	var charts : Array[IndividualChart] = []
 	for i in amtOfCharts:
 		var individual_chart : IndividualChart = IndividualChart.new()
 		
-		var name_length : int = sign_uint32(reader.get_32())
+		var name_length : int = int(reader.get_32())
 		individual_chart.Name = reader.get_buffer(name_length).get_string_from_utf8()
-		individual_chart.Lanes = sign_uint32(reader.get_32())
+		individual_chart.Lanes = int(reader.get_32())
 		
-		var target_switch_count : int = sign_uint32(reader.get_32())
+		var target_switch_count : int = int(reader.get_32())
 		var target_switches : Array[TargetSwitch] = []
 		for j in target_switch_count:
 			var target_switch : TargetSwitch = TargetSwitch.new()
 			target_switch.Time = reader.get_float()
 			
-			var ts_name_length : int = sign_uint32(reader.get_32())
+			var ts_name_length : int = int(reader.get_32())
 			target_switch.Name = reader.get_buffer(ts_name_length).get_string_from_utf8()
 			target_switches.push_back(target_switch)
 		
 		individual_chart.Switches = target_switches
 		
-		var sv_change_count : int = sign_uint32(reader.get_32())
+		var sv_change_count : int = int(reader.get_32())
 		var sv_changes : Array[SvChange] = []
 		for j in sv_change_count:
 			var sv_change : SvChange = SvChange.new()
@@ -46,56 +46,56 @@ static func convert(reader : FileAccess) -> RubiChart:
 		
 		individual_chart.SvChanges = sv_changes
 		
-		var note_count : int = sign_uint32(reader.get_32())
+		var note_count : int = int(reader.get_32())
 		var notes : Array[NoteData] = []
 		for j in note_count:
 			var note : NoteData = NoteData.new()
 			var serialized_type : int = reader.get_8()
 			note.Time = reader.get_float()
-			note.Lane = sign_uint32(reader.get_32())
+			note.Lane = int(reader.get_32())
 			
 			match serialized_type:
 				1: # Typed tap note
-					note.Type = note_types[sign_uint32(reader.get_32())]
+					note.Type = note_types[int(reader.get_32())]
 				2: # Tap note with params
-					var param_count : int = sign_uint32(reader.get_32())
+					var param_count : int = int(reader.get_32())
 					for k in param_count:
-						var param_name_length : int = sign_uint32(reader.get_32())
+						var param_name_length : int = int(reader.get_32())
 						var param_name : StringName = reader.get_buffer(param_name_length).get_string_from_utf8()
-						var param_value_length : int = sign_uint32(reader.get_32())
+						var param_value_length : int = int(reader.get_32())
 						var param_value : Variant = bytes_to_var(reader.get_buffer(param_value_length))
 						note.Parameters.set(param_name, param_value)
 				3: # Typed tap note with params
-					note.Type = note_types[sign_uint32(reader.get_32())]
-					var param_count : int = sign_uint32(reader.get_32())
+					note.Type = note_types[int(reader.get_32())]
+					var param_count : int = int(reader.get_32())
 					for k in param_count:
-						var param_name_length : int = sign_uint32(reader.get_32())
+						var param_name_length : int = int(reader.get_32())
 						var param_name : StringName = reader.get_buffer(param_name_length).get_string_from_utf8()
-						var param_value_length : int = sign_uint32(reader.get_32())
+						var param_value_length : int = int(reader.get_32())
 						var param_value : Variant = bytes_to_var(reader.get_buffer(param_value_length))
 						note.Parameters.set(param_name, param_value)
 				4: # Normal hold note
 					note.Length = reader.get_float()
 				5: # Typed hold note
 					note.Length = reader.get_float()
-					note.Type = note_types[sign_uint32(reader.get_32())]
+					note.Type = note_types[int(reader.get_32())]
 				6: # Hold note with params
 					note.Length = reader.get_float()
-					var param_count : int = sign_uint32(reader.get_32())
+					var param_count : int = int(reader.get_32())
 					for k in param_count:
-						var param_name_length : int = sign_uint32(reader.get_32())
+						var param_name_length : int = int(reader.get_32())
 						var param_name : StringName = reader.get_buffer(param_name_length).get_string_from_utf8()
-						var param_value_length : int = sign_uint32(reader.get_32())
+						var param_value_length : int = int(reader.get_32())
 						var param_value : Variant = bytes_to_var(reader.get_buffer(param_value_length))
 						note.Parameters.set(param_name, param_value)
 				7: # Typed hold note with parameters
 					note.Length = reader.get_float()
-					note.Type = note_types[sign_uint32(reader.get_32())]
-					var param_count : int = sign_uint32(reader.get_32())
+					note.Type = note_types[int(reader.get_32())]
+					var param_count : int = int(reader.get_32())
 					for k in param_count:
-						var param_name_length : int = sign_uint32(reader.get_32())
+						var param_name_length : int = int(reader.get_32())
 						var param_name : StringName = reader.get_buffer(param_name_length).get_string_from_utf8()
-						var param_value_length : int = sign_uint32(reader.get_32())
+						var param_value_length : int = int(reader.get_32())
 						var param_value : Variant = bytes_to_var(reader.get_buffer(param_value_length))
 						note.Parameters.set(param_name, param_value)
 		
@@ -105,6 +105,3 @@ static func convert(reader : FileAccess) -> RubiChart:
 	chart.Charts = charts
 	
 	return chart
-
-static func sign_uint32(unsigned : int):
-	return (unsigned + 1 << 31) % 1 << 32 - 1 << 31
